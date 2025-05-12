ca2f7a9aed0032f8d6529585ddea72af7370bbaa
Pranit Gore <pranitgore0605@gmail.com>
2025-05-12 20:05:30 +0530

    setup.py commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    # This function is used to return a list of requirements
+    
+    with open(file_path) as file_obj:
+        requirements = []
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        
+        if HYPEN_DOT in requirements:
+            requirements.remove(HYPEN_DOT)
+    return requirements
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Pranit Gore',
+    author_email = 'pranitgore0605@gmail.com',
+    prackages = find_packages(),
+    install_requirements = get_requirements('requirements.txt'),
+)
